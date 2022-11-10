aefa13d698084a7ff00495c8ea954295180afde7
noeulgim <noeul3240@gmail.com>
2022-11-10 16:30:41 +0900

    update

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+$(function(){
+  var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+});
